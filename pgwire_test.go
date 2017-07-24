cockroachdb/cockroach

sql: do not use leased table descriptors modified during a transaction
This change makes a sql transaction cache table descriptors modified
by it and use them within the transaction without leasing the descriptors
themselves. This allows a transaction to consistently see its own changes.

The need for this change is described here:

https://github.com/cockroachdb/cockroach/blob/master/docs/RFCS/table_descriptor_lease.md#accommodating-schema-changes-within-transactions

In the future, we plan to decouple the user transaction from the
transaction acquiring a lease. This change will prevent a
deadlock between an open transaction that has modified a descriptor
and the other transaction that is attempting to acquire a lease
on the descriptor. This change breaks the dependency between these
two transactions, by having the former not depend on the latter.

related to #16727

pkg/sql/pgwire/pgwire_test.go
@@ -498,26 +498,13 @@ func TestPGPrepareWithCreateDropInTxn(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		stmt, err := tx.Prepare(`INSERT INTO d.kv (k,v) VALUES ($1, $2);`)
-		if err != nil {
-			t.Fatal(err)
+		if _, err := tx.Prepare(`
+INSERT INTO d.kv (k,v) VALUES ($1, $2);
+`); !testutils.IsError(err, "table \"d.kv\" does not exist") {
+			t.Fatalf("err = %v", err)
 		}
 
-		// INSERT works because it is using a cached descriptor that is leased.
-		res, err := stmt.Exec('c', 'd')
-		if err != nil {
-			t.Fatal(err)
-		}
-		stmt.Close()
-		affected, err := res.RowsAffected()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if affected != 1 {
-			t.Fatalf("unexpected number of rows affected: %d", affected)
-		}
-
-		if err := tx.Commit(); err != nil {
+		if err := tx.Rollback(); err != nil {
 			t.Fatal(err)
 		}
 	}

mcuadros/pgwire

pgwire_test.go
@@ -498,26 +498,13 @@ func TestPGPrepareWithCreateDropInTxn(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		stmt, err := tx.Prepare(`INSERT INTO d.kv (k,v) VALUES ($1, $2);`)
-		if err != nil {
-			t.Fatal(err)
+		if _, err := tx.Prepare(`
+INSERT INTO d.kv (k,v) VALUES ($1, $2);
+`); !testutils.IsError(err, "relation \"d.kv\" does not exist") {
+			t.Fatalf("err = %v", err)
 		}
 
-		// INSERT works because it is using a cached descriptor that is leased.
-		res, err := stmt.Exec('c', 'd')
-		if err != nil {
-			t.Fatal(err)
-		}
-		stmt.Close()
-		affected, err := res.RowsAffected()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if affected != 1 {
-			t.Fatalf("unexpected number of rows affected: %d", affected)
-		}
-
-		if err := tx.Commit(); err != nil {
+		if err := tx.Rollback(); err != nil {
 			t.Fatal(err)
 		}
 	}